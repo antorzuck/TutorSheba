98eebeba8d5bd453d4ef7247614276a770019d6c
Antor Mahmud <82134898+antorzuck@users.noreply.github.com>
2021-11-05 15:59:39 +0600

    added static root

--- TutorSheba/settings.py
+++ TutorSheba/settings.py
@@ -119,6 +119,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
